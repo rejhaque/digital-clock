f6b6d11292a883c6323b1b7f3158188e468e9e0f
rejhaque <rij.rezwan@gmail.com>
2022-10-13 20:58:17 +0600

    functionality done

---
+++ app.js
@@ -0,0 +1,30 @@
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const amPm = document.getElementById("ampm");
+
+function updateClock(){
+     let h = new Date(). getHours();
+     let m = new Date(). getMinutes();
+     let s = new Date(). getSeconds();
+     let ampm = "AM"
+
+     if(h > 12){
+          h = h - 12;
+          ampm = "PM";
+     }
+
+     h = h < 10 ? "0" + h : h;
+
+
+     hour.innerText = h;
+     min.innerText = m;
+     sec.innerText = s;
+     amPm.innerText = ampm;
+
+     setTimeout(()=>{
+          updateClock();
+     },1000);
+}
+
+updateClock();
