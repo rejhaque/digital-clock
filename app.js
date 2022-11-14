35c4f3f200a0488c76adec6f0f8d0deb5e4e0519
rejhaque <rij.rezwan@gmail.com>
2022-11-14 16:30:34 +0600

    Now working properly

--- app.js
+++ app.js
@@ -3,30 +3,29 @@ const min = document.getElementById("min");
 const sec = document.getElementById("sec");
 const amPm = document.getElementById("ampm");
 
-function updateClock(){
-     let h = new Date(). getHours();
-     let m = new Date(). getMinutes();
-     let s = new Date(). getSeconds();
-     let ampm = "AM"
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
 
-     if(h > 12){
-          h = h - 12;
-          ampm = "PM";
-     }
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
 
-     h = h < 10 ? "0" + h : h;
-     m = m < 10 ? "0" + m : m;
-     h = s < 10 ? "0" + s : s;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
+  hour.innerText = h;
+  min.innerText = m;
+  sec.innerText = s;
+  amPm.innerText = ampm;
 
-     hour.innerText = h;
-     min.innerText = m;
-     sec.innerText = s;
-     amPm.innerText = ampm;
-
-     setTimeout(()=>{
-          updateClock();
-     },1000);
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
 }
 
 updateClock();
